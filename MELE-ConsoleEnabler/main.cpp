75a39307921650311db25121b8963dede055a79f
Erik JS <erikjs_br@hotmail.com>
2021-05-18 17:30:39 -0300

    Console Enabler update
    Algorithm updated to be compatible with latest game update.
    Using pattern search; should be compatible with future updates.

--- MELE-ConsoleEnabler/main.cpp
+++ MELE-ConsoleEnabler/main.cpp
@@ -1,26 +1,57 @@
 #include <windows.h>
 #include <stdio.h>
 
+unsigned char pattern1[64] = {
+	0x69, 0x00, 0x6E, 0x00, 0x74, 0x00, 0x41, 0x00, 0x57, 0x00, 0x6F, 0x00,
+	0x72, 0x00, 0x6C, 0x00, 0x64, 0x00, 0x49, 0x00, 0x6E, 0x00, 0x66, 0x00,
+	0x6F, 0x00, 0x65, 0x00, 0x78, 0x00, 0x65, 0x00, 0x63, 0x00, 0x49, 0x00,
+	0x73, 0x00, 0x53, 0x00, 0x68, 0x00, 0x69, 0x00, 0x70, 0x00, 0x70, 0x00,
+	0x69, 0x00, 0x6E, 0x00, 0x67, 0x00, 0x42, 0x00, 0x75, 0x00, 0x69, 0x00,
+	0x6C, 0x00, 0x64, 0x00
+};
+
+bool DataCompare(const BYTE* OpCodes, const BYTE* Mask, const char* StrMask)
+{
+	while (*StrMask)
+	{
+		if(*StrMask == 'x' && *OpCodes != *Mask )
+			return false;
+		++StrMask;
+		++OpCodes;
+		++Mask;
+	}
+	return true;
+}
+
+DWORD64 FindPattern(DWORD64 StartAddress, DWORD64 CodeLen, BYTE* Mask, const char* StrMask, unsigned short ignore)
+{
+	unsigned short Ign = 0;
+	DWORD64 i = 0;
+	while (Ign <= ignore)
+	{
+		if(DataCompare((BYTE*)(StartAddress + i++), Mask,StrMask))
+			++Ign;
+		else if (i>=CodeLen)
+			return 0;
+	}
+	return StartAddress + i - 1;
+}
+
 DWORD WINAPI Start(LPVOID lpParam)
 {
     Sleep(1000);
-    DWORD64 target1 = 0, target2 = 0, target3 = 0;
-    char filename[FILENAME_MAX];
-    HMODULE hMain = GetModuleHandle(NULL);
-    GetModuleFileName(hMain, filename, sizeof(filename));
-    int gamenumberindex = strlen(filename) - 5;
-    if(filename[gamenumberindex] == '1') target1 = 0x14168E358;
-    else if(filename[gamenumberindex] == '2') target1 = 0x141679458;
-    else if(filename[gamenumberindex] == '3') target1 = 0x1417BD088;
-    target2 = target1 + 0x20;
-    target3 = target2 + 0x200;
+    DWORD64 bResult1 = FindPattern(0x140000000, 0x2000000, pattern1, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 0);
+    if (bResult1 == 0)
+        return 0;
+    DWORD64 qResult2 = FindPattern(0x140000000, 0x2000000, (BYTE*)&bResult1, "xxxxxxxx", 0);
+    DWORD64 *mTarget = (DWORD64*)qResult2;
     DWORD dwProtect = NULL;
-    VirtualProtect((void*)(target1), 0x4, PAGE_READWRITE, &dwProtect );
-    VirtualProtect((void*)(target2), 0x4, PAGE_READWRITE, &dwProtect );
-    *(DWORD64*)(target1) = *(DWORD64*)(target3);
-    *(DWORD64*)(target2) = *(DWORD64*)(target3);
-    VirtualProtect((void*)(target1), 0x4, dwProtect, &dwProtect );
-    VirtualProtect((void*)(target2), 0x4, dwProtect, &dwProtect );
+    VirtualProtect((void*)(&mTarget[1]), sizeof(DWORD64), PAGE_READWRITE, &dwProtect );
+    VirtualProtect((void*)(&mTarget[5]), sizeof(DWORD64), PAGE_READWRITE, &dwProtect );
+    mTarget[1] = mTarget[3];
+    mTarget[5] = mTarget[3];
+    VirtualProtect((void*)(&mTarget[1]), sizeof(DWORD64), dwProtect, &dwProtect );
+    VirtualProtect((void*)(&mTarget[5]), sizeof(DWORD64), dwProtect, &dwProtect );
     return 0;
 }
 
